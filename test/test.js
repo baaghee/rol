490c067eb6c0a547619c04dd41a652500b19c6a4
iuthisaam <iuthisaam@eww.(none)>
2013-12-09 20:32:04 +0500

    testing

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 var chai = require('chai');
 var expect = chai.expect;
+var mongoose = require("mongoose");
+
+var Types = require("../lib/cms/Types")
+
 chai.should();
 
 describe('Database', function(){
@@ -8,10 +12,66 @@ describe('Database', function(){
 	});
 });
 
-describe('connection', function(){
-	it('#must establish a connection', function(){
-	
-	});
+describe('Data Types',function(){
+    //common to all datatypes
+    it("must exist", function(){
+        expect(Types).to.exist;
+    });
+    describe("server", function(){
+        it("#must exist", function(){
+            for(var Type in Types) {
+                var type = Types[Type];
+                expect(type.server).to.exist
+            }
+        });
+        it("should have a valid mongoose schema type", function(){
+            for(var Type in Types) {
+                var type = Types[Type];
+                var schema = type.server.schema;
+                expect(schema).to.exist;
+                new mongoose.Schema({ name: schema }); 
+            }
+        });
+    });
+    describe("client", function(){
+        describe("element", function(){
+            it("must exist", function(){
+                for(var Type in Types) {
+                    var type = Types[Type];
+                    expect(type.client).to.exist
+                }
+            });
+            it("should have options as first argument", function(){
+            });
+            it("must return a valid html markup string", function(){
+            });
+        });
+        
+        describe("render", function(){
+            it("should exist");
+            it("should have valid html markup string as first argument")
+            it("should have json data as second element")
+            it("should return a DOM element")
+        });
+        
+        describe("serialize", function(){
+            it("should return a valid string or an object");
+        });
+    });
+    
+    /**
+     ** String
+     **
+     */
+     describe("Text", function(){
+        describe("options", function(){
+            it("must have ")
+        });
+     });
+});
+
+describe("Client", function(){
+	it("generates client file");
 });
 
 describe('Data Source', function(){
